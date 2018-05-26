01bef89986bceb9eee75bb9a4471029ff8dacc19
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-05-26 12:09:33 +0200

    Add IEqualityComparer for distinction of the contacts, implementing IEquatable<> for comparer purposes

--- ContactsApp/ContactEntry.cs
+++ ContactsApp/ContactEntry.cs
@@ -6,20 +6,18 @@ using System.Threading.Tasks;
 
 namespace ContactsApp
 {
-    public class ContactEntry
+    public class ContactEntry : IEquatable<ContactEntry>
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public string TelephoneNumber { get; set; }
 
-        public static IComparer<ContactEntry> SurnameComparator =
-            Comparer<ContactEntry>.Create((entry1, entry2) => entry1.Surname.CompareTo(entry2.Surname));
-
-        public static IComparer<ContactEntry> NameComparator =
-            Comparer<ContactEntry>.Create((entry1, entry2) => entry1.Name.CompareTo(entry2.Name));
+        public static IEqualityComparer<ContactEntry>
+            TelephoneComparer = EqualityComparer<ContactEntry>.Default;
 
         public ContactEntry()
         {
+            Name = Surname = TelephoneNumber = string.Empty;
         }
 
         public ContactEntry(string surname, string telephoneNumber)
@@ -38,6 +36,26 @@ namespace ContactsApp
         public override string ToString()
         {
             return $"{Surname} {Name} {TelephoneNumber}";
+        }
+
+        public bool Equals(ContactEntry other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(TelephoneNumber, other.TelephoneNumber);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ContactEntry) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (TelephoneNumber != null ? TelephoneNumber.GetHashCode() : 0);
         }
     }
 }
