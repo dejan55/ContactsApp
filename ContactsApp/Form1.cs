97e3185360b81b0129f72d78e8d73c0939fcd759
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-05-26 18:33:34 +0200

    Refactor of the old code

--- ContactsApp/Form1.cs
+++ ContactsApp/Form1.cs
@@ -3,247 +3,96 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsApp.Properties;
 
 namespace ContactsApp
 {
     public partial class Form1 : Form
     {
+        private static readonly Color BlueColor = Color.FromArgb(27, 93, 198);
+        private static readonly Color WhiteColor = Color.FromArgb(255, 255, 255);
+        private static readonly Color BlackColor = Color.FromArgb(35, 35, 35);
+        private static readonly Label[] Labels = new Label[26];
+        private static readonly Font CustomFont = new Font("Verdana", 22F, FontStyle.Regular,
+            GraphicsUnit.Point, ((byte) (0)));
+
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = BlackColor;
         }
 
-
-        private void label24_MouseEnter(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Label lbl = (Label)sender;
-            lbl.ForeColor = Color.Aqua;
+            var left = 20;
+            var top = 20;
+            var labelSize = 60;
+            var labelOffset = 60;
+
+            for (int i = 1, x = left, y = left; i <= 26; i++, x += labelOffset)
+            {
+                Labels[i - 1] = new Label();
+
+                Labels[i - 1].AutoSize = true;
+                Labels[i - 1].BackColor = BlackColor;
+                Labels[i - 1].Font = CustomFont;
+                Labels[i - 1].ForeColor = SystemColors.Control;
+                Labels[i - 1].Location = new Point(x, y);
+                Labels[i - 1].Margin = new Padding(4, 0, 4, 0);
+                Labels[i - 1].Name = $"label{(char) ('A' + (i - 1))}";
+                Labels[i - 1].Size = new Size(labelSize, labelSize);
+                Labels[i - 1].TabIndex = 0;
+                Labels[i - 1].Text = $"{(char) ('A' + (i - 1))}";
+                Labels[i - 1].MouseEnter += this.label_MouseEnter;
+                Labels[i - 1].MouseLeave += this.label_MouseLeave;
+
+                Controls.Add(Labels[i - 1]);
+
+                if (i != 0 && i % 4 == 0)
+                {
+                    x = left - labelOffset;
+                    y += labelOffset;
+                }
+            }
+
+            this.Width = 5 * labelOffset - left;
+            this.Height = 8 * labelSize + top;
         }
 
-        private void label24_MouseLeave(object sender, EventArgs e)
+        private void label_MouseEnter(object sender, EventArgs e)
         {
-            Label lbl = (Label)sender;
-            lbl.ForeColor = Color.White;
+            var lbl = (Label) sender;
+            lbl.ForeColor = BlueColor;
+        }
+
+        private void label_MouseLeave(object sender, EventArgs e)
+        {
+            var lbl = (Label) sender;
+            lbl.ForeColor = WhiteColor;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.A)
-            {
-                labelA.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.B)
-            {
-                labelB.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.C)
-            {
-                labelC.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.D)
-            {
-                labelD.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.E)
-            {
-                labelE.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.F)
-            {
-                labelF.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.G)
-            {
-                labelG.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.H)
-            {
-                labelH.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.I)
-            {
-                labelI.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.J)
-            {
-                labelJ.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.K)
-            {
-                labelK.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.L)
-            {
-                labelL.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.M)
-            {
-                labelM.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.N)
-            {
-                labelN.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.O)
-            {
-                labelO.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.P)
-            {
-                labelP.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.Q)
-            {
-                labelQ.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.R)
-            {
-                labelR.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.S)
-            {
-                labelS.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.T)
-            {
-                labelT.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.U)
-            {
-                labelU.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.V)
-            {
-                labelV.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.W)
-            {
-                labelW.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.X)
-            {
-                labelX.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.Y)
-            {
-                labelY.ForeColor = Color.Aqua;
-            }
-            if (e.KeyCode == Keys.Z)
-            {
-                labelZ.ForeColor = Color.Aqua;
-            }
+            if (e.KeyCode.ToString().Length != 1)
+                return;
+
+            var key = e.KeyCode.ToString()[0];
+            Labels[key - 'A'].ForeColor = BlueColor;
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.A)
-            {
-                labelA.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.B)
-            {
-                labelB.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.C)
-            {
-                labelC.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.D)
-            {
-                labelD.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.E)
-            {
-                labelE.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.F)
-            {
-                labelF.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.G)
-            {
-                labelG.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.H)
-            {
-                labelH.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.I)
-            {
-                labelI.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.J)
-            {
-                labelJ.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.K)
-            {
-                labelK.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.L)
-            {
-                labelL.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.M)
-            {
-                labelM.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.N)
-            {
-                labelN.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.O)
-            {
-                labelO.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.P)
-            {
-                labelP.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.Q)
-            {
-                labelQ.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.R)
-            {
-                labelR.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.S)
-            {
-                labelS.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.T)
-            {
-                labelT.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.U)
-            {
-                labelU.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.V)
-            {
-                labelV.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.W)
-            {
-                labelW.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.X)
-            {
-                labelX.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.Y)
-            {
-                labelY.ForeColor = Color.White;
-            }
-            if (e.KeyCode == Keys.Z)
-            {
-                labelZ.ForeColor = Color.White;
-            }
+            if (e.KeyCode.ToString().Length != 1)
+                return;
+
+            var key = e.KeyCode.ToString()[0];
+            Labels[key - 'A'].ForeColor = WhiteColor;
         }
     }
 }
