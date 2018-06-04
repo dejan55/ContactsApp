72c86149b542e2021a0e3f3ee6eb73faf6ace833
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-06-04 23:32:04 +0200

    Done edit feature for a contact

--- ContactsApp/ContactDetails.Designer.cs
+++ ContactsApp/ContactDetails.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.txtFirstName = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave3 = new System.Windows.Forms.Button();
+            this.btnSave2 = new System.Windows.Forms.Button();
+            this.btnSave1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtNumber = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -48,9 +52,15 @@
             this.txtFirstName.ReadOnly = true;
             this.txtFirstName.Size = new System.Drawing.Size(177, 22);
             this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.Leave += new System.EventHandler(this.txt_Leave);
+            this.txtFirstName.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txt_MouseDoubleClick);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnSave3);
+            this.groupBox1.Controls.Add(this.btnSave2);
+            this.groupBox1.Controls.Add(this.btnSave1);
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.txtNumber);
             this.groupBox1.Controls.Add(this.label2);
@@ -59,10 +69,51 @@
             this.groupBox1.Controls.Add(this.txtFirstName);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(458, 177);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.Size = new System.Drawing.Size(458, 183);
+            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Details";
+            // 
+            // btnSave3
+            // 
+            this.btnSave3.Location = new System.Drawing.Point(346, 126);
+            this.btnSave3.Name = "btnSave3";
+            this.btnSave3.Size = new System.Drawing.Size(75, 23);
+            this.btnSave3.TabIndex = 9;
+            this.btnSave3.Text = "Save";
+            this.btnSave3.UseVisualStyleBackColor = true;
+            this.btnSave3.Visible = false;
+            // 
+            // btnSave2
+            // 
+            this.btnSave2.Location = new System.Drawing.Point(346, 84);
+            this.btnSave2.Name = "btnSave2";
+            this.btnSave2.Size = new System.Drawing.Size(75, 23);
+            this.btnSave2.TabIndex = 8;
+            this.btnSave2.Text = "Save";
+            this.btnSave2.UseVisualStyleBackColor = true;
+            this.btnSave2.Visible = false;
+            // 
+            // btnSave1
+            // 
+            this.btnSave1.Location = new System.Drawing.Point(346, 42);
+            this.btnSave1.Name = "btnSave1";
+            this.btnSave1.Size = new System.Drawing.Size(75, 23);
+            this.btnSave1.TabIndex = 7;
+            this.btnSave1.Text = "Save";
+            this.btnSave1.UseVisualStyleBackColor = true;
+            this.btnSave1.Visible = false;
+            this.btnSave1.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(290, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "*Double click the text boxes to edit";
             // 
             // label3
             // 
@@ -80,6 +131,8 @@
             this.txtNumber.ReadOnly = true;
             this.txtNumber.Size = new System.Drawing.Size(177, 22);
             this.txtNumber.TabIndex = 4;
+            this.txtNumber.Leave += new System.EventHandler(this.txt_Leave);
+            this.txtNumber.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txt_MouseDoubleClick);
             // 
             // label2
             // 
@@ -97,6 +150,8 @@
             this.txtLastName.ReadOnly = true;
             this.txtLastName.Size = new System.Drawing.Size(177, 22);
             this.txtLastName.TabIndex = 2;
+            this.txtLastName.Leave += new System.EventHandler(this.txt_Leave);
+            this.txtLastName.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txt_MouseDoubleClick);
             // 
             // label1
             // 
@@ -112,7 +167,7 @@
             this.btnDelete.Location = new System.Drawing.Point(38, 213);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(114, 44);
-            this.btnDelete.TabIndex = 2;
+            this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
@@ -122,7 +177,7 @@
             this.btnSendSMS.Location = new System.Drawing.Point(317, 213);
             this.btnSendSMS.Name = "btnSendSMS";
             this.btnSendSMS.Size = new System.Drawing.Size(114, 44);
-            this.btnSendSMS.TabIndex = 3;
+            this.btnSendSMS.TabIndex = 0;
             this.btnSendSMS.Text = "Send a message";
             this.btnSendSMS.UseVisualStyleBackColor = true;
             // 
@@ -138,6 +193,7 @@
             // 
             // ContactDetails
             // 
+            this.AcceptButton = this.btnSendSMS;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnQuit;
@@ -150,7 +206,8 @@
             this.MaximizeBox = false;
             this.Name = "ContactDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "ContactDetails";
+            this.Text = "Contact Details";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ContactDetails_FormClosed);
             this.Load += new System.EventHandler(this.ContactDetails_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -170,5 +227,9 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnSendSMS;
         private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave3;
+        private System.Windows.Forms.Button btnSave2;
+        private System.Windows.Forms.Button btnSave1;
     }
 }
--- ContactsApp/ContactDetails.cs
+++ ContactsApp/ContactDetails.cs
@@ -15,6 +15,8 @@ namespace ContactsApp
         public ContactEntry SelectedContact { get; set; }
         public IDictionary<char, ISet<ContactEntry>> Contacts { get; set; }
 
+        private bool isEdited;
+
         public ContactDetails(ContactEntry selectedContact,
             IDictionary<char, ISet<ContactEntry>> contacts)
         {
@@ -22,6 +24,7 @@ namespace ContactsApp
             SelectedContact = selectedContact;
             Contacts = contacts;
             this.Text = $"Details for {SelectedContact}";
+            isEdited = false;
         }
 
         private void ContactDetails_Load(object sender, EventArgs e)
@@ -36,14 +39,14 @@ namespace ContactsApp
             if (MessageBox.Show($"Are you sure you want to delete {SelectedContact}?", "Delete a contact",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
             {
-                foreach (var contactSets in Contacts.Values)
+                foreach (var contactSet in Contacts.Values)
                 {
-                    var contact = contactSets.FirstOrDefault(
+                    var contact = contactSet.FirstOrDefault(
                         c => c.TelephoneNumber == SelectedContact.TelephoneNumber);
 
                     if (contact != null)
                     {
-                        contactSets.Remove(contact);
+                        contactSet.Remove(contact);
                         break;
                     }
                 }
@@ -54,7 +57,122 @@ namespace ContactsApp
 
         private void btnQuit_Click(object sender, EventArgs e)
         {
-            DialogResult = DialogResult.Cancel;
+            if (isEdited)
+                DialogResult = DialogResult.Yes;
+            else
+                DialogResult = DialogResult.Cancel;
+        }
+
+        private void txt_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            try
+            {
+                var textBox = (TextBox) sender;
+                var button = GetButton(textBox.Name);
+
+                if (button == null)
+                {
+                    Console.WriteLine("Button not found!");
+                    return;
+                }
+
+                button.Visible = true;
+                button.Click += this.btnSave_Click;
+                textBox.ReadOnly = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        private void txt_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                var textBox = (TextBox) sender;
+                var button = GetButton(textBox.Name);
+
+                if (button == null)
+                    return;
+
+                textBox.ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var button = (Button) sender;
+                var textbox = GetTextBox(button.Name[button.Name.Length - 1]);
+
+                if (textbox == null)
+                    return;
+
+                foreach (var contactSet in Contacts.Values)
+                {
+                    var contact = contactSet.FirstOrDefault(
+                        c => c.TelephoneNumber == SelectedContact.TelephoneNumber);
+
+                    if (contact != null)
+                    {
+                        contact.FirstName = txtFirstName.Text;
+                        contact.LastName = txtLastName.Text;
+                        contact.TelephoneNumber = txtNumber.Text;
+                        isEdited = true;
+                        break;
+                    }
+                }
+
+                button.Visible = false;
+                textbox.ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        private Button GetButton(string textBoxName)
+        {
+            if (textBoxName == "txtFirstName")
+                return btnSave1;
+            if (textBoxName == "txtLastName")
+                return btnSave2;
+            if (textBoxName == "txtNumber")
+                return btnSave3;
+            return null;
+        }
+
+        private TextBox GetTextBox(char buttonNum)
+        {
+            if (int.TryParse(buttonNum.ToString(), out var num))
+            {
+                if (num == 1)
+                    return txtFirstName;
+                if (num == 2)
+                    return txtLastName;
+                if (num == 3)
+                    return txtNumber;
+            }
+
+            return null;
+        }
+
+        private void ContactDetails_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (isEdited)
+                DialogResult = DialogResult.Yes;
+            else
+                DialogResult = DialogResult.Cancel;
         }
     }
 }
