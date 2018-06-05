78e0b1290b3241949d2144b261fcef77d5182115
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-06-05 12:43:12 +0200

    Initial commit

---
+++ ContactsApp/SendSMS.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ContactsApp
+{
+    partial class SendSMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblChars = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.AcceptsReturn = true;
+            this.txtMessage.AcceptsTab = true;
+            this.txtMessage.Location = new System.Drawing.Point(12, 12);
+            this.txtMessage.MaxLength = 765;
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(402, 162);
+            this.txtMessage.TabIndex = 2;
+            this.txtMessage.Text = "Enter your message here...";
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            this.txtMessage.Enter += new System.EventHandler(this.txtMessage_Enter);
+            this.txtMessage.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyUp);
+            this.txtMessage.Leave += new System.EventHandler(this.txtMessage_Leave);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 198);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(78, 33);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.CausesValidation = false;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(336, 198);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(78, 33);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblChars
+            // 
+            this.lblChars.AutoSize = true;
+            this.lblChars.Location = new System.Drawing.Point(194, 177);
+            this.lblChars.Name = "lblChars";
+            this.lblChars.Size = new System.Drawing.Size(16, 17);
+            this.lblChars.TabIndex = 3;
+            this.lblChars.Text = "0";
+            // 
+            // SendSMS
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(426, 248);
+            this.Controls.Add(this.lblChars);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SendSMS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Send SMS";
+            this.Load += new System.EventHandler(this.SendSMS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblChars;
+    }
+}
---
+++ ContactsApp/SendSMS.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsApp
+{
+    public partial class SendSMS : Form
+    {
+        public ContactEntry SelectedContact { get; set; }
+        private const string ApiKey = "jNutD8o0fsQ-gLQQLhMp4a7it6H1uKHCleEkiIPna0";
+
+        public SendSMS(ContactEntry selectedContact)
+        {
+            InitializeComponent();
+            SelectedContact = selectedContact;
+            this.Text = $"Send a SMS to {SelectedContact.FirstName}";
+        }
+
+        private void SendSMS_Load(object sender, EventArgs e)
+        {
+            txtMessage.ForeColor = Color.Gray;
+
+            btnSend.Enabled = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var status = SendSms();
+            if (status.Contains("\"status\":\"success\"") && !status.Contains("\"warnings\":"))
+            {
+                MessageBox.Show("Your message was sent successfully", "Successful",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Your message was not sent successfully", "Unsuccessful",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DialogResult = DialogResult.Cancel;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to discard the message?", "Discard the message",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                DialogResult = DialogResult.Cancel;
+            else
+                DialogResult = DialogResult.None;
+        }
+
+        private void txtMessage_Enter(object sender, EventArgs e)
+        {
+            txtMessage.Text = string.Empty;
+            txtMessage.ForeColor = Color.Black;
+            txtMessage.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void txtMessage_Leave(object sender, EventArgs e)
+        {
+            txtMessage.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void txtMessage_KeyUp(object sender, KeyEventArgs e)
+        {
+            //                     [space][tab] [new line]
+            if (txtMessage.Text.Trim(' ', '\t', '\r', '\n') == string.Empty)
+                btnSend.Enabled = false;
+            else
+                btnSend.Enabled = true;
+        }
+
+        public string SendSms()
+        {
+            string result;
+            var number = $"00389 {SelectedContact.TelephoneNumber.Substring(1)}";
+            var message = txtMessage.Text;
+            var sender = "Kostadin";
+
+            var url =
+                "https://api.txtlocal.com/send/?" +
+                "apikey=" + ApiKey +
+                "&numbers=" + number +
+                "&message=" + message +
+                "&sender=" + sender;
+            Console.WriteLine($"URL: {url}");
+
+            var objRequest = WebRequest.Create(url) as HttpWebRequest;
+            if (objRequest == null)
+                return string.Empty;
+
+            objRequest.Method = "POST";
+            objRequest.ContentLength = Encoding.UTF8.GetByteCount(url);
+            objRequest.ContentType = "application/x-www-form-urlencoded";
+            try
+            {
+                using (var myWriter = new StreamWriter(objRequest.GetRequestStream()))
+                {
+                    myWriter.Write(url);
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            var objResponse = objRequest.GetResponse() as HttpWebResponse;
+            if (objResponse == null)
+                return string.Empty;
+
+            using (var sr = new StreamReader(objResponse.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            Console.WriteLine($"RESULT: {result}");
+            return result;
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            var numChars = txtMessage.Text.Length;
+            int numMessages = 1;
+            if (numChars > 160)
+                numMessages += (numChars + 7) / 160;
+
+            if (numMessages > 1)
+                lblChars.Text = $"{numChars} ({numMessages})";
+            else lblChars.Text = $"{numChars}";
+        }
+    }
+}
