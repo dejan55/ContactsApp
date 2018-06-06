386b3bf67fe9df7004b8bf98fd241deed33a0cae
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-06-06 23:33:27 +0200

    Add MouseEnter & MouseLeave events

--- ContactsApp/ContactDetails.Designer.cs
+++ ContactsApp/ContactDetails.Designer.cs
@@ -50,11 +50,11 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(122, 34);
-            this.txtFirstName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFirstName.Location = new System.Drawing.Point(163, 42);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.ReadOnly = true;
-            this.txtFirstName.Size = new System.Drawing.Size(134, 20);
+            this.txtFirstName.Size = new System.Drawing.Size(177, 22);
             this.txtFirstName.TabIndex = 0;
             this.txtFirstName.Click += new System.EventHandler(this.txt_Click);
             this.txtFirstName.Leave += new System.EventHandler(this.txt_Leave);
@@ -73,11 +73,11 @@
             this.groupBox1.Controls.Add(this.txtLastName);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtFirstName);
-            this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(344, 149);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(459, 183);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Details";
@@ -85,71 +85,75 @@
             // btnSave3
             // 
             this.btnSave3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave3.Location = new System.Drawing.Point(283, 102);
-            this.btnSave3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave3.Location = new System.Drawing.Point(377, 126);
+            this.btnSave3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave3.Name = "btnSave3";
-            this.btnSave3.Size = new System.Drawing.Size(56, 19);
-            this.btnSave3.TabIndex = 9;
+            this.btnSave3.Size = new System.Drawing.Size(75, 23);
+            this.btnSave3.TabIndex = 6;
             this.btnSave3.Text = "Save";
             this.btnSave3.UseVisualStyleBackColor = true;
             this.btnSave3.Visible = false;
             this.btnSave3.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave3.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btnSave3.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // btnSave2
             // 
             this.btnSave2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave2.Location = new System.Drawing.Point(283, 68);
-            this.btnSave2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave2.Location = new System.Drawing.Point(377, 84);
+            this.btnSave2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave2.Name = "btnSave2";
-            this.btnSave2.Size = new System.Drawing.Size(56, 19);
-            this.btnSave2.TabIndex = 8;
+            this.btnSave2.Size = new System.Drawing.Size(75, 23);
+            this.btnSave2.TabIndex = 6;
             this.btnSave2.Text = "Save";
             this.btnSave2.UseVisualStyleBackColor = true;
             this.btnSave2.Visible = false;
             this.btnSave2.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave2.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btnSave2.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // btnSave1
             // 
             this.btnSave1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave1.Location = new System.Drawing.Point(283, 33);
-            this.btnSave1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave1.Location = new System.Drawing.Point(377, 41);
+            this.btnSave1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave1.Name = "btnSave1";
-            this.btnSave1.Size = new System.Drawing.Size(56, 19);
-            this.btnSave1.TabIndex = 7;
+            this.btnSave1.Size = new System.Drawing.Size(75, 23);
+            this.btnSave1.TabIndex = 6;
             this.btnSave1.Text = "Save";
             this.btnSave1.UseVisualStyleBackColor = true;
             this.btnSave1.Visible = false;
             this.btnSave1.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave1.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btnSave1.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(218, 131);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(291, 161);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(120, 9);
+            this.label4.Size = new System.Drawing.Size(168, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "*Double click the text boxes to edit";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 102);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(5, 126);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 13);
-            this.label3.TabIndex = 5;
+            this.label3.Size = new System.Drawing.Size(134, 17);
+            this.label3.TabIndex = 6;
             this.label3.Text = "Telephone Number:";
             // 
             // txtNumber
             // 
-            this.txtNumber.Location = new System.Drawing.Point(122, 102);
-            this.txtNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumber.Location = new System.Drawing.Point(163, 126);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNumber.Name = "txtNumber";
             this.txtNumber.ReadOnly = true;
-            this.txtNumber.Size = new System.Drawing.Size(134, 20);
-            this.txtNumber.TabIndex = 4;
+            this.txtNumber.Size = new System.Drawing.Size(177, 22);
+            this.txtNumber.TabIndex = 2;
             this.txtNumber.Click += new System.EventHandler(this.txt_Click);
             this.txtNumber.Leave += new System.EventHandler(this.txt_Leave);
             this.txtNumber.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txt_MouseDoubleClick);
@@ -158,21 +162,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(5, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 3;
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 6;
             this.label2.Text = "Last Name:";
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(122, 68);
-            this.txtLastName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLastName.Location = new System.Drawing.Point(163, 84);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.ReadOnly = true;
-            this.txtLastName.Size = new System.Drawing.Size(134, 20);
-            this.txtLastName.TabIndex = 2;
+            this.txtLastName.Size = new System.Drawing.Size(177, 22);
+            this.txtLastName.TabIndex = 1;
             this.txtLastName.Click += new System.EventHandler(this.txt_Click);
             this.txtLastName.Leave += new System.EventHandler(this.txt_Leave);
             this.txtLastName.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txt_MouseDoubleClick);
@@ -181,42 +184,45 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 37);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(5, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 1;
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 6;
             this.label1.Text = "First Name:";
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(28, 173);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Location = new System.Drawing.Point(37, 213);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(86, 36);
+            this.btnDelete.Size = new System.Drawing.Size(115, 44);
             this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            this.btnDelete.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btnDelete.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // btnSendSMS
             // 
-            this.btnSendSMS.Location = new System.Drawing.Point(238, 173);
-            this.btnSendSMS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSendSMS.Location = new System.Drawing.Point(317, 213);
+            this.btnSendSMS.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSendSMS.Name = "btnSendSMS";
-            this.btnSendSMS.Size = new System.Drawing.Size(86, 36);
+            this.btnSendSMS.Size = new System.Drawing.Size(115, 44);
             this.btnSendSMS.TabIndex = 0;
             this.btnSendSMS.Text = "Send a message";
             this.btnSendSMS.UseVisualStyleBackColor = true;
             this.btnSendSMS.Click += new System.EventHandler(this.btnSendSMS_Click);
+            this.btnSendSMS.MouseEnter += new System.EventHandler(this.btn_MouseEnter);
+            this.btnSendSMS.MouseLeave += new System.EventHandler(this.btn_MouseLeave);
             // 
             // btnQuit
             // 
             this.btnQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnQuit.Location = new System.Drawing.Point(420, 219);
-            this.btnQuit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnQuit.Location = new System.Drawing.Point(560, 270);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(8, 8);
+            this.btnQuit.Size = new System.Drawing.Size(11, 10);
             this.btnQuit.TabIndex = 4;
             this.btnQuit.UseVisualStyleBackColor = true;
             this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
@@ -227,24 +233,23 @@
             // 
             // ContactDetails
             // 
-            this.AcceptButton = this.btnSendSMS;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnQuit;
-            this.ClientSize = new System.Drawing.Size(362, 236);
+            this.ClientSize = new System.Drawing.Size(483, 290);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.btnSendSMS);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "ContactDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Contact Details";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ContactDetails_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ContactDetails_FormClosed);
             this.Load += new System.EventHandler(this.ContactDetails_Load);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ContactDetails_MouseMove);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
