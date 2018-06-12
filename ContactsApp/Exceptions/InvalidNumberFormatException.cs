d7c48fe8065a78c37ceeabfea4de233b072d945e
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-06-12 18:32:08 +0200

    Initial commit

---
+++ ContactsApp/Exceptions/InvalidNumberFormatException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContactsApp.Exceptions
+{
+    public class InvalidNumberFormatException : Exception
+    {
+        public InvalidNumberFormatException() : base("Invalid number")
+        {
+        }
+
+        public InvalidNumberFormatException(string message) : base(message)
+        {
+        }
+    }
+}
