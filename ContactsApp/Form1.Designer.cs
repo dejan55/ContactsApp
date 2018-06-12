c52cfdec10539b6e006dd457ac27fe4476eb4ca3
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-06-12 18:00:38 +0200

    Change the form text

--- ContactsApp/Form1.Designer.cs
+++ ContactsApp/Form1.Designer.cs
@@ -282,7 +282,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "5";
+            this.Text = "Contacts";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
