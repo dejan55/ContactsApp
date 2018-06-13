84795668579374323f319f0d5516ff6e1722aa0e
jovanakov <jovannakov7@gmail.com>
2018-06-13 23:16:23 +0200

    Added variable to store the date when the message was sent

--- ContactsApp/Message.cs
+++ ContactsApp/Message.cs
@@ -7,11 +7,13 @@ namespace ContactsApp
     {
         public string Sender { get; set; }
         public string Msg { get; set; }
+        public DateTime date { get; set; }
 
-        public Message(string sender, string msg)
+        public Message(string sender, string msg, DateTime dt)
         {
             this.Sender = sender;
             this.Msg = msg;
+            date = dt;
         }
 
         public override string ToString()
@@ -22,6 +24,11 @@ namespace ContactsApp
         public string MessageToString()
         {
             return Msg;
+        }
+
+        public string DateToString()
+        {
+            return date.ToString();
         }
     }
 }
